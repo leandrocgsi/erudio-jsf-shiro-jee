3ae365f90f504384166dc2ccb0a47e90dc8a9f4c
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2016-04-04 20:25:24 -0300

    Change Swagger URL

--- src/main/webapp/swagger-ui.js
+++ src/main/webapp/swagger-ui.js
@@ -25134,7 +25134,7 @@ SwaggerUi.Views.HeaderView = Backbone.View.extend({
 
   showPetStore: function(){
     this.trigger('update-swagger-ui', {
-      url:'http://localhost:8080/rest/api-docs'
+      url:'http://localhost:8080/jsf-shiro-jee/rest/api-docs'
     });
   },
 
